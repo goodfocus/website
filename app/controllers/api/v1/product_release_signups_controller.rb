39b3a18863c70837ce5d250bc848642d03cf408d
sublimestyle90 <mcollins43@saddleback.edu>
2021-03-23 12:33:59 -0700

    - adjusted error handling to show w/ bootstrap styles - adjusted controller to return success in proper context - refactored/simplified homepage.scss

--- app/controllers/api/v1/product_release_signups_controller.rb
+++ app/controllers/api/v1/product_release_signups_controller.rb
@@ -2,11 +2,11 @@ class Api::V1::ProductReleaseSignupsController < ApplicationController
   before_action :sanitize_params
 
   def create
-    product_release_signup = ProductReleaseSignup.create!(product_release_signup_params)
-    if product_release_signup
+    product_release_signup = ProductReleaseSignup.create(product_release_signup_params)
+    if product_release_signup and product_release_signup.errors.size == 0
       render json: product_release_signup
     else
-      render json: product_release_signup.errors
+      render json: product_release_signup.errors, status: 422
     end
   end
 
--- app/javascript/components/SignupForm.jsx
+++ app/javascript/components/SignupForm.jsx
@@ -18,47 +18,62 @@ export class SignupForm extends React.Component {
         }
 
         bindMethodsToComponent(this,
-            this.handleInputChange,
-            this.handleSubmit
+            this.handleEmailChange,
+            this.handleSubmit,
+            this.hasError
         )
     }
 
-    handleInputChange(event) {
-        const inputEle = event.target;
-        this.setState({[inputEle.name]: inputEle.value});
+    hasError() {
+        return this.state.errorMsg.length > 0;
+    }
+
+    handleEmailChange(event) {
+        const email = event.target.value;
+        this.setState({email, errorMsg: ''});
     }
 
     handleSubmit(event) {
         event.preventDefault();
         const email = this.state.email;
 
-        // TODO: separate into .env files and use env contextual url
         $.ajax(`/api/v1/product_release_signups/create`, {
             method: 'POST',
             contentType: 'application/json',
             data: JSON.stringify({email}),
-            success: (response) => {
-                console.log(response);
+            success: () => {
+                this.setState({isComplete: true});
             },
-            error: (error) => {
-                console.log(error)
+            error: (err) => {
+                // if expected error, display it, else display generic error message
+                let errorMsg;
+                try {
+                    errorMsg = `Email ${err.responseJSON.email[0]}, please try again.`;
+                } catch {
+                    errorMsg = 'Email could not be registered, please try again later.';
+                }
+                this.setState({errorMsg});
             }
         })
     }
 
     render() {
         return (
-            <form className="form-floating" method="POST" onSubmit={this.handleSubmit}>
-                <input
-                    id="email"
-                    className="form-control"
-                    type="email"
-                    name="email"
-                    placeholder="name@example.com"
-                    value={this.state.email}
-                    onChange={this.handleInputChange}
-                    required/>
-                <label htmlFor="email">Email address</label>
+            <form method="POST" onSubmit={this.handleSubmit}>
+                <div className="form-floating">
+                    <input
+                        id="email"
+                        className={`form-control${ this.hasError() ?' is-invalid' : ''}`}
+                        type="email"
+                        placeholder="name@example.com"
+                        value={this.state.email}
+                        onChange={this.handleEmailChange}
+                        required/>
+                    <label htmlFor="email">Email address</label>
+                    <div className="invalid-feedback">
+                        {this.state.errorMsg}
+                    </div>
+                </div>
                 <button className="btn" type="submit">Sign up</button>
             </form>
         );
